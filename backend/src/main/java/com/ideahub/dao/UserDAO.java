7e8ef49dbba8e4bc56504e7d38af3a9ccc84b213
Mitsuo Takaki <mitsuotakaki@gmail.com>
2016-02-20 11:08:03 -0800

    Checking in hibernate integration test utility class

--- backend/src/main/java/com/ideahub/dao/UserDAO.java
+++ backend/src/main/java/com/ideahub/dao/UserDAO.java
@@ -1,8 +1,11 @@
 package com.ideahub.dao;
 // Generated Feb 20, 2016 12:32:19 AM by Hibernate Tools 4.3.1
 
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 
+import com.google.common.base.Optional;
 import com.ideahub.model.User;
 
 import io.dropwizard.hibernate.AbstractDAO;
@@ -15,5 +18,11 @@ import io.dropwizard.hibernate.AbstractDAO;
 public class UserDAO extends AbstractDAO<User> {
     public UserDAO(final SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    public Optional<User> findByEmail(final String email) {
+        final Criteria criteria = this.criteria()
+                .add(Restrictions.eq("email", email));
+        return Optional.fromNullable(this.uniqueResult(criteria));
     }
 }
---
+++ backend/src/test/java/com/ideahub/dao/HibernateDAOTestUtil.java
@@ -0,0 +1,123 @@
+package com.ideahub.dao;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ManagedSessionContext;
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.service.ServiceRegistry;
+import org.joda.time.DateTimeZone;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.MetricRegistry;
+
+import io.dropwizard.db.ManagedPooledDataSource;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import lombok.Getter;
+
+/**
+ * Test utility class that will setup hibernate and {@link SessionFactory}. For
+ * each test it will spawn an in memory database so every test is completely
+ * isolated from each other.
+ *
+ * @author mitsuo
+ *
+ */
+public class HibernateDAOTestUtil {
+    @Getter
+    private SessionFactory sessionFactory;
+    @Getter
+    private Session session;
+    private final List<Class<?>> entitiesClass;
+
+    public HibernateDAOTestUtil(final List<Class<?>> entitiesClass) {
+        // Getting rid of too many warnings in the console.
+        final Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.WARN);
+
+        this.entitiesClass = entitiesClass;
+
+        this.setupCanonicalTimezone();
+        this.setupSessionAndTransaction();
+    }
+
+    private void setupCanonicalTimezone() {
+        // Setting canonical timezone to UTC.
+        final TimeZone defaultTimeZone = TimeZone.getTimeZone("UTC");
+        TimeZone.setDefault(defaultTimeZone);
+        DateTimeZone.setDefault(DateTimeZone.forTimeZone(defaultTimeZone));
+    }
+
+    private void setupSessionAndTransaction() {
+        this.sessionFactory = this.buildSessionFactory();
+        this.openSession();
+    }
+
+    public Session openSession() {
+        this.session = this.sessionFactory.openSession();
+        ManagedSessionContext.bind(this.session);
+        return this.session;
+    }
+
+    /**
+     * Setups hibernate and builds database connection and
+     * {@link SessionFactory}.
+     *
+     * @return A {@link SessionFactory} used to connect to the in memory
+     *         database.
+     */
+    private SessionFactory buildSessionFactory() {
+        final PoolProperties poolConfig = new PoolProperties();
+        poolConfig.setDriverClassName("org.hsqldb.jdbcDriver");
+        poolConfig.setUrl("jdbc:hsqldb:mem:test");
+        poolConfig.setUsername("sa");
+        poolConfig.setPassword("");
+        final ManagedPooledDataSource dataSource = new ManagedPooledDataSource(poolConfig,
+                new MetricRegistry());
+
+        final Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.autoReconnect", "true");
+
+        final Configuration configuration = new Configuration();
+        configuration.setProperty(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "managed");
+        configuration.setProperty(AvailableSettings.GENERATE_STATISTICS, "false");
+        configuration.setProperty(AvailableSettings.SHOW_SQL, "true");
+        configuration.setProperty("jadira.usertype.autoRegisterUserTypes", "true");
+        configuration.setProperty(AvailableSettings.DIALECT, "org.hibernate.dialect.HSQLDialect");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+        // Registering the entity classes.
+        for (final Class<?> entityClass : this.entitiesClass) {
+            configuration.addAnnotatedClass(entityClass);
+        }
+
+        final DatasourceConnectionProviderImpl connectionProvider = new DatasourceConnectionProviderImpl();
+        connectionProvider.setDataSource(dataSource);
+        connectionProvider.configure(properties);
+
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .addService(ConnectionProvider.class, connectionProvider)
+                .applySettings(properties)
+                .build();
+
+        return configuration.buildSessionFactory(registry);
+    }
+
+    public void closeSessionAndDropSchema() {
+        if (this.session.isOpen()) {
+            this.session.createSQLQuery("TRUNCATE SCHEMA PUBLIC AND COMMIT").executeUpdate();
+            this.session.close();
+        }
+        this.sessionFactory.close();
+    }
+}
