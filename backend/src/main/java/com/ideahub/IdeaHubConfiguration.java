0c7f9150a741900e236f52d53f0ea4fcb6422eee
kylekury <ktg0@hotmail.com>
2016-02-20 00:43:59 -0800

    Added DB to config.

--- backend/src/main/java/com/ideahub/IdeaHubConfiguration.java
+++ backend/src/main/java/com/ideahub/IdeaHubConfiguration.java
@@ -1,7 +1,16 @@
 package com.ideahub;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
 public class IdeaHubConfiguration extends Configuration {
-
+    @Valid
+    @NotNull
+    @JsonProperty("database")
+    private final DataSourceFactory database = new DataSourceFactory();
 }
