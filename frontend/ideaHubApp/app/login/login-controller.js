224be150e685530eb26b7de05f0daf616423e5a4
Rocky Lee <rocky.lee@payfirma.com>
2016-02-20 22:17:17 -0800

    AngularJs Structure for frontend

---
+++ frontend/ideaHubApp/app/login/login-controller.js
@@ -0,0 +1,22 @@
+(function() {
+
+	'use strict';
+
+    angular
+        .module('ideaHubApp')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$q', '$scope'];
+
+    function LoginController($q, $scope) {
+
+    	console.log("LoginController");
+
+
+
+
+
+
+    }
+
+})();
